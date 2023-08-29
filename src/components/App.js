9d8785a55ffe4241692f8db09c035a3b3abea8b6
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-30 02:33:18 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ const App = () => {
   let list=['pitake','ladoo','idli','payasam','rabdi']
   return (
     <div id="main">
-      <ol key={'relativeList'}>
+      <ol id={'relativeList'}>
       {
         list.map((element,index)=>{
           return (
-            <li key={'relativeListItem'+index+1}>{element}</li>
+            <li id={'relativeListItem'+index+1}>{element}</li>
           )
         })
       }
